53c59dab2a4f32a616ccf2a3edbd3af5364a09ce
tomieric <tomieric@gmail.com>
2016-10-22 11:00:47 +0800

    更新bower目录

--- dist/static/js/config.js
+++ dist/static/js/config.js
@@ -1,7 +1,7 @@
-/*! gulp-browersync-boilerplate v1.0.0
+/*! gulp-browersync-boilerplate v1.0.1
 *  by tommyshao
 *  (c) 2014-2016 tommyshao
-* updated on 2016-10-21
+* updated on 2016-10-22
 *  Licensed under MIT
 */
- !function(){var config="undefined"==typeof window.webConfig?{}:window.webConfig;require.config({baseUrl:function(){return config.baseUrl||"./js"}(),urlArgs:function(){return config.ver?"ver="+config.ver:"debug="+(new Date).getTime()}(),paths:{components:"./components",jquery:"components/jquery/dist/jquery.min",scripts:function(){return config.scripts?config.scripts:"/scripts"}()},shim:{}})}();
+ !function(){var config="undefined"==typeof window.webConfig?{}:window.webConfig;require.config({baseUrl:function(){return config.baseUrl||"./js"}(),urlArgs:function(){return config.ver?"ver="+config.ver:"debug="+(new Date).getTime()}(),paths:{vendor:"./vendor",jquery:"vendor/jquery/dist/jquery.min",scripts:function(){return config.scripts?config.scripts:"/scripts"}()},shim:{}})}();
--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -1,7 +1,7 @@
-/*! gulp-browersync-boilerplate v1.0.0
+/*! gulp-browersync-boilerplate v1.0.1
 *  by tommyshao
 *  (c) 2014-2016 tommyshao
-* updated on 2016-10-21
+* updated on 2016-10-22
 *  Licensed under MIT
 */
  define(["jquery"],function($){$(function(){console&&console.log&&console.log("DomReady!")})});
